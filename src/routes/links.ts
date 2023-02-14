8a5f73fe7329b206d030f2613c2de525bf56023b
Adebiyi Blessing <yoyoplenty@gmail.com>
2023-02-14 16:51:55 +0100

    initial commit for the admin dashboard

---
+++ src/routes/links.ts
@@ -0,0 +1,44 @@
+export const links = [
+  {
+    url: "/app/dashboard",
+    i_class: "bx bxs-dashboard",
+    link_title: "Dashboard",
+    key: 0,
+  },
+  {
+    url: "products",
+    i_class: "bx bxs-shopping-bag-alt ",
+    link_title: "Products",
+    key: 1,
+  },
+  {
+    url: "users",
+    i_class: "bx bxs-user ",
+    link_title: "Users",
+    key: 2,
+  },
+  {
+    url: "sales",
+    i_class: "bx bxs-wallet ",
+    link_title: "Sales",
+    key: 3,
+  },
+  {
+    url: "deliveries",
+    i_class: "bx bxs-truck ",
+    link_title: "Deliveries",
+    key: 4,
+  },
+  {
+    url: "messages",
+    i_class: "bx bxs-message-dots ",
+    link_title: "Messages",
+    key: 5,
+  },
+  {
+    url: "settings",
+    i_class: "bx bxs-cog ",
+    link_title: "Settings",
+    key: 6,
+  },
+];
